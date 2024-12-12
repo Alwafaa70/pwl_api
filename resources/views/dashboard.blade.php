60f7507890198caf9d85a85570aadcd5cae8a5b9
alwafaa70 <m.alwafa.k@gmail.com>
2024-12-12 19:51:03 +0700

    alwafaa boy

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,12 +9,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-lg sm:rounded-lg border border-gray-200 dark:border-gray-700">
                 <div class="p-6 text-gray-900 dark:text-gray-100 text-center">
-                    <h3 class="text-xl font-bold mb-4">{{ __("Selamat Datang di Sistem Informasi Perpus Alvalfa") }}</h3>
+                    <h3 class="text-xl font-bold mb-4">{{ __("Selamat Datang di Sistem Informasi Perpustakaan ALWAFAA") }}</h3>
                     <p class="text-lg">{{ __("Halo, ".Auth()->user()->name."!") }}</p>
                     <p class="text-sm mt-2 text-gray-500 dark:text-gray-400">Semoga harimu menyenangkan!</p>
-                    <div class="mt-6 text-blue-500 hover:text-blue-600">
-                        {{ __('Jelajahi Sistem') }}
-                    </div>
                 </div>
             </div>
         </div>
